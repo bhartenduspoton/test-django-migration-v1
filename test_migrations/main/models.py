47bbc8bd4c53acabceaff1bfcbc02c1bdeceffe0
bhartendu <bhartendu@spotonsoft.com>
2017-04-19 23:48:52 +0530

    f1 initail

--- test_migrations/main/models.py
+++ test_migrations/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length = 20)
+    n = models.CharField(max_length = 20)
 
-
-    
+class Teacher(models.Model):
+    name = models.CharField(max_length = 20)
--- test_migrations/test_migrations/settings.py
+++ test_migrations/test_migrations/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'test_migrations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test_migration',
+        'USER': 'postgres',
+        'PASSWORD': 'bharti',
+        'HOST': 'localhost'
+       
     }
 }
 
